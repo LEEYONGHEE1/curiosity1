b316c5d41061a7a7d1f53e2c1dfbfefc7920bc1d
LEEYONGHEE1 <lyh990104@naver.com>
2022-10-09 02:11:05 +0900

    그래프

--- curiosity/app/src/main/java/com/project/curiosity/fragment/GraphFragment.kt
+++ curiosity/app/src/main/java/com/project/curiosity/fragment/GraphFragment.kt
@@ -151,11 +151,12 @@ class GraphFragment : Fragment() {
             }
         }
 
-        viewModel.specificDataError.observe(this, Observer {
-            it.getContentIfNotHandled()?.let {
-                Toast.makeText(requireActivity(), "a", Toast.LENGTH_SHORT).show()
+        (activity as MainActivity).viewModel.specificErrorData.observe(viewLifecycleOwner) {
+            it.getContentIfNotHandled()?.let{
+                Log.d("처리중" , "데이터 변함!")
+                requireActivity().runOnUiThread { Toast.makeText(context, "선택한 날짜의 정보가 없습니다.", Toast.LENGTH_SHORT).show() }
             }
-        })
+        }
 
         initLineChart()
         setDataToLineChartHumidity()
@@ -168,24 +169,6 @@ class GraphFragment : Fragment() {
         return binding.root
     }
 
-    open class Event<out T>(private val content: T) {
-        var hasBeenHandled = false
-            private set
-
-        fun getContentIfNotHandled(): T? {
-            return if (hasBeenHandled) { // 이벤트가 이미 처리 되었다면
-                null // null을 반환하고,
-            } else { // 그렇지 않다면
-                hasBeenHandled = true // 이벤트가 처리되었다고 표시한 후에
-                content // 값을 반환합니다.
-            }
-        }
-
-        /**
-         * 이벤트의 처리 여부에 상관 없이 값을 반환합니다.
-         */
-        fun peekContent(): T = content
-    }
 
     private fun getDate(view:View){
         if(view.id.toString() == binding.TemperatureButton.id.toString())
